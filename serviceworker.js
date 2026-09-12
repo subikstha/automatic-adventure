ea2a8fddb42dae3f894d51b5ece4559a71c5b665
subikstha <subsatch@gmail.com>
2026-09-12 14:38:54 +0545

    feat: serving and caching resources; ;

--- serviceworker.js
+++ serviceworker.js
@@ -1,7 +1,33 @@
-console.log("I am a service worker ready to serve")
+console.log("I am a service worker ready to serve anything from the cache")
+const url = ["/", "app.js", "styles.css", "https://fonts.gstatic.com/s/materialicons/v67/flUhRq6tzZclQEJ-Vdg-IuiaDsNcIhQ8tQ.woff2"]
 
-globalThis.addEventListener('fetch', (event) => {
-    console.log("HTTP requested", event.request.url)
-
-    event.respondWith(new Response("Hey! hehehe"))
+// Whenever this service worker is installed, store the things in the url array in the cache
+// This is known as prefetching the assets
+globalThis.addEventListener("install", event => {
+    caches.open("pwa").then(cache => {
+        cache.addAll(url)
+    })
 })
+
+globalThis.addEventListener("fetch", event => {
+    event.respondWith(
+        // the match method accepts string, url or a request object
+        caches.match(event.request) // Searching in the cache
+            .then(response => {
+                if(response) {
+                    // If the request is found in the cache
+                    // If it is a cache hit it gives the response to respondWith
+                    return response // Cache hit
+                } else {
+                    // We need to go to the network
+                    return fetch(event.request); // Cache miss
+                }
+            })
+    )
+})
+// NOTE: Cache storage is key based storage with the key being the HTTP request
+// globalThis.addEventListener('fetch', (event) => {
+//     console.log("HTTP requested", event.request.url)
+
+//     event.respondWith(new Response("Hey! hehehe"))
+// })
